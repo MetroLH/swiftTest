bcbea6e7eb3f8f83da9a92a9558e405e312034bc
SubWar <576650401@qq.com>
2017-11-13 09:55:14 +0800

    update

--- swiftTest/ViewController.swift
+++ swiftTest/ViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        showAlert(msg: "嘿嘿嘿嘿")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    func showAlert(msg:String) {
+        NSLog("这是个啥%@", msg);
+    }
 
 }
 
